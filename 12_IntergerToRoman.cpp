4d025b7d79a144252bc9db5a4ed46d7414e4aba9
gemrey13 <gemreyranola@gmail.com>
2026-05-01 16:35:21 +0800

    Add commented-out alternative implementation for romanToInt function

--- 12_IntergerToRoman.cpp
+++ 12_IntergerToRoman.cpp
@@ -118,3 +118,31 @@ int main()
 
     return 0;
 }
+
+
+/* 
+    int romanToInt(std::string s) {
+        int res = 0;
+        std::unordered_map<char, int> roman = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10}, 
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        for (int i = 0; i < s.size() - 1; i++) {
+
+            // scanning two roman then if the left is less than the right roman it subtract
+            if (roman[s[i]] < roman[s[i + 1]]) {
+                res -= roman[s[i]];
+            } else {
+                res += roman[s[i]];
+            }
+        }
+                        // this is a last value
+        return res + roman[s[s.size() - 1]];        
+    }
+*/
